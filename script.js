bc26659d388d2a60c0879088a889b45592c154eb
gracechua93 <chua.faithboy@gmail.com>
2024-11-20 13:52:11 +0800

    added and update js

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('contact-page');
+
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  alert("Form submitted successfully!");
+});
